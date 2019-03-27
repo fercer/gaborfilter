4690223caa326b79b1c07465437e247a2f1b96d4
fercer <iie.fercer@gmail.com>
2019-03-27 12:22:38 -0600

    Changes to compile instructions

--- examples/Compile_instructions.bat
+++ examples/Compile_instructions.bat
@@ -1,2 +1,2 @@
 if "%1" == "fourier" cl /Fo:.\build\test_fouriertransform.obj /ID:\Apps\fftw\include .\src\test_fouriertransform.c D:\Apps\fftw\lib\fftw3.lib /link /OUT:.\bin\test_fouriertransform.exe
-if "%1" == "gabor"  cl /Fo:.\build\test_gaborfilter.obj /ID:\Apps\gaborfilter\include /ID:\Apps\fftw\include .\src\test_gaborfilter.c D:\Apps\fftw\lib\fftw3.lib D:\Apps\gaborfilter\lib\libgabor.lib /link /OUT:.\bin\test_gaborfilter.exe
+if "%1" == "gabor"  cl /Fo:.\build\test_gaborfilter.obj /I..\include /IC:\fftw\include .\src\test_gaborfilter.c C:\fftw\lib\fftw3.lib ..\lib\libgabor.lib /link /OUT:.\bin\test_gaborfilter.exe
